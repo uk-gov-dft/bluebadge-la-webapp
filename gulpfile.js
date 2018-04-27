56ca0d538fdac1b5b6a50aa7cc0e9d298bc79ccf
Ali Ashik <ali.ashik@valtech.co.uk>
2018-04-27 13:57:56 +0100

    refactored gulp env and created layout templates

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,11 @@ const rollup = require('gulp-better-rollup');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 
-/** ---------------------------------------
- * Configs and Paths
---------------------------------------- **/
+/** --------------------------------------------------
+
+ * Configs and paths
+
+-------------------------------------------------- **/
 
 const DEFAULT_ENV = "development";
 const BASE_PATH = "./src/main/resources";
@@ -27,32 +29,38 @@ const PATH = {
 };
 
 
-/** ---------------------------------------
- * Utility Functions for Environment
---------------------------------------- **/
+/** --------------------------------------------------
+
+ * Utility functions for identifying environment
+
+-------------------------------------------------- **/
 
 const getEnv = () => {
+	
 	const params = global.process.argv;
 
-	for(let index in params){
-		const param = String(params[index]);
+	for(let param of params){
 		
 		if(param.includes("--env")) {
+
 			const env = param.split('=')[1];
 			return env ? env : DEFAULT_ENV;
+
 		}
 	}
 
 	return DEFAULT_ENV;
 }
 
-const isProd = () => getEnv() === "production";
-const isDev = () => getEnv() === "development";
+const isProd = getEnv() === "production";
+const isDev = getEnv() === "development";
 
 
-/** ---------------------------------------
- * Default Task
---------------------------------------- **/
+/** --------------------------------------------------
+
+ * Default task
+
+-------------------------------------------------- **/
 
 gulp.task('default', ['sass', 'js']);
 
@@ -67,13 +75,14 @@ gulp.task('sass', ['clean:css'], () => {
 	return gulp.src(PATH.sourceAssets.sass)
 		//.pipe(linter)
 		.pipe(sass({
-			// Added so its easier to import GDS toolkit inside sass file
+			// Added so its easier to import GDS toolkit
+			// inside sass file
 			includePaths: 'node_modules'
 		}))
-		.pipe(gulpIf(isDev(), sourcemaps.init()))
+		.pipe(gulpIf(isDev, sourcemaps.init()))
 		.pipe(autoprefixer()) // needs to go upto iE8 ?
-		.pipe(gulpIf(isDev(), sourcemaps.write('./')))
-		.pipe(gulpIf(isProd(), cssnano({
+		.pipe(gulpIf(isDev, sourcemaps.write('./')))
+		.pipe(gulpIf(isProd, cssnano({
 			discardComments: {
 				removeAll: true
 			}
@@ -87,9 +96,9 @@ gulp.task('sass', ['clean:css'], () => {
 gulp.task('js', () => {
 	gulp.src(PATH.sourceAssets.js)
 		//.pipe(linter)
-		.pipe(gulpIf(isDev(), sourcemaps.init()))
+		.pipe(gulpIf(isDev, sourcemaps.init()))
 		//.pipe(rollup({plugins: [babel()]}, 'umd'))
 		.pipe(rollup())
-		.pipe(gulpIf(isDev(), sourcemaps.write('./')))
+		.pipe(gulpIf(isDev, sourcemaps.write('./')))
 		.pipe(gulp.dest(PATH.compiledAssets.js))
 });
