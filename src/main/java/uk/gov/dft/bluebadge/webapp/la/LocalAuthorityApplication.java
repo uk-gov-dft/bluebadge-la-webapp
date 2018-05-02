2767dbc68fc18f987964993db8cfe7709a56ac09
Miguel Gil <miguel.gil_garcia@valtech.co.uk>
2018-05-02 10:40:16 +0100

    Comments in code review addressed

--- src/main/java/uk/gov/dft/bluebadge/webapp/la/LocalAuthorityApplication.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/LocalAuthorityApplication.java
@@ -2,10 +2,7 @@ package uk.gov.dft.bluebadge.webapp.la;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
--- src/main/java/uk/gov/dft/bluebadge/webapp/la/config/MessagesConfig.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/config/MessagesConfig.java
@@ -4,44 +4,37 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 @Configuration
 public class MessagesConfig extends WebMvcConfigurerAdapter {
-    @Bean
-    public MessageSource messageSource() {
-        final ReloadableResourceBundleMessageSource messageSource =
-                new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        messageSource.setFallbackToSystemLocale(false);
-        messageSource.setCacheSeconds(0);
-        return messageSource;
-    }
-/*
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
-        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
-        return sessionLocaleResolver;
-    }
+  @Bean
+  public MessageSource messageSource() {
+    final ReloadableResourceBundleMessageSource messageSource =
+        new ReloadableResourceBundleMessageSource();
+    messageSource.setBasename("messages");
+    messageSource.setFallbackToSystemLocale(false);
+    messageSource.setCacheSeconds(0);
+    return messageSource;
+  }
+  /*
+  @Bean
+  public LocaleResolver localeResolver() {
+      SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+      sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+      return sessionLocaleResolver;
+  }
 
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-        localeChangeInterceptor.setParamName("lang");
-        return localeChangeInterceptor;
-    }
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+      LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+      localeChangeInterceptor.setParamName("lang");
+      return localeChangeInterceptor;
+  }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
-    }
-    */
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+      registry.addInterceptor(localeChangeInterceptor());
+  }
+  */
 }
-
