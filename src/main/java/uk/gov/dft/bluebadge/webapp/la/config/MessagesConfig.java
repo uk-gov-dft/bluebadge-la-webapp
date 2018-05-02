8fd0c3c3b2a6ed8f8ae68943ee9211391ba70a9e
Miguel Gil <miguel.gil_garcia@valtech.co.uk>
2018-05-02 11:05:33 +0100

    Improve locale resolver code

--- src/main/java/uk/gov/dft/bluebadge/webapp/la/config/MessagesConfig.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/config/MessagesConfig.java
@@ -4,10 +4,20 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import javax.websocket.Session;
+import java.util.Locale;
 
 @Configuration
-public class MessagesConfig extends WebMvcConfigurerAdapter {
+public class MessagesConfig implements WebMvcConfigurer {
+  
   @Bean
   public MessageSource messageSource() {
     final ReloadableResourceBundleMessageSource messageSource =
@@ -17,24 +27,34 @@ public class MessagesConfig extends WebMvcConfigurerAdapter {
     messageSource.setCacheSeconds(0);
     return messageSource;
   }
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+    localeResolver.setDefaultLocale(Locale.ENGLISH);
+    return localeResolver;
+  }
+
   /*
   @Bean
   public LocaleResolver localeResolver() {
-      SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
-      sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
-      return sessionLocaleResolver;
-  }
+    SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+      localeResolver.setDefaultLocale(Locale.ENGLISH);
+      return localeResolver;
+  }*/
 
+  /*
   @Bean
   public LocaleChangeInterceptor localeChangeInterceptor() {
       LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
       localeChangeInterceptor.setParamName("lang");
       return localeChangeInterceptor;
-  }
+  }*/
+
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
       registry.addInterceptor(localeChangeInterceptor());
   }
-  */
+
 }
