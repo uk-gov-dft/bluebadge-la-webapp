f5b7911d4a32a23f452ce8bfbbf9df48fbe5410d
Ali Ashik <ali.ashik@valtech.co.uk>
2018-06-08 10:15:23 +0100

    #BBB-50 text change

--- src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/SetPasswordControllerImpl.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/SetPasswordControllerImpl.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import uk.gov.dft.bluebadge.webapp.la.controller.request.SetPasswordFormRequest;
-import uk.gov.dft.bluebadge.webapp.la.controller.utils.TemplateModelUtils;
-import uk.gov.dft.bluebadge.webapp.la.service.SignInService;
 import uk.gov.dft.bluebadge.webapp.la.service.UserService;
 
 @Controller
