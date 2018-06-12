00fd4f2b42b8d1518dbe86e1a8bff4c8d4653b00
Ali Ashik <ali.ashik@valtech.co.uk>
2018-06-12 12:58:06 +0100

    #BBB-50 added controller scaffold.

--- src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/SetPasswordControllerImpl.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/SetPasswordControllerImpl.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import uk.gov.dft.bluebadge.model.usermanagement.UserResponse;
 import uk.gov.dft.bluebadge.webapp.la.controller.request.SetPasswordFormRequest;
 import uk.gov.dft.bluebadge.webapp.la.service.UserService;
 
@@ -17,6 +18,8 @@ public class SetPasswordControllerImpl implements SetPasswordController {
 
   public static final String URL_SET_PASSWORD = "/set***REMOVED***";
   public static final String TEMPLATE_SET_PASSWORD = "set***REMOVED***";
+  public static final String REDIRECT_URL_SIGN_IN = "redirect:" + SignInControllerImpl.URL_SIGN_IN;
+
   private UserService userService;
 
   @Autowired
@@ -33,12 +36,19 @@ public class SetPasswordControllerImpl implements SetPasswordController {
   }
 
   @PostMapping(URL_SET_PASSWORD)
-  public String setPassword(
-      @Valid @ModelAttribute("formRequest") final SetPasswordFormRequest formRequest,
-      BindingResult bindingResult,
-      Model model,
-      HttpSession session) {
+  public String setPassword(@Valid @ModelAttribute("formRequest") final
+                              SetPasswordFormRequest formRequest, BindingResult bindingResult,
+                              Model model, HttpSession session) {
 
-    return TEMPLATE_SET_PASSWORD;
+    try {
+
+      // UserResponse response = this.userService.updatePassword;
+
+      return REDIRECT_URL_SIGN_IN;
+
+    } catch (Exception exception) {
+
+      return TEMPLATE_SET_PASSWORD;
+    }
   }
 }
