93149dd7bee556faa3d8318f8f2e49b5711114cd
Ali Ashik <ali.ashik@valtech.co.uk>
2018-06-20 14:25:21 +0100

    #BBB-86 made tweaks to templates and css

--- src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/request/ManageUsersFormRequest.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/request/ManageUsersFormRequest.java
@@ -2,8 +2,6 @@ package uk.gov.dft.bluebadge.webapp.la.controller.request;
 
 import lombok.Data;
 
-import java.util.Objects;
-
 @Data
 public class ManageUsersFormRequest {
   private String search;
