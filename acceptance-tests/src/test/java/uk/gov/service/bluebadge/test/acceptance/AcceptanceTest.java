e1f65e3b5ae1a12dea27e515bd0c822e15e6ed76
sampathmahavithana <sampath.mahavithana@valtech.co.uk>
2018-06-27 12:03:50 +0100

    Reverted email checks back to accept uppercases

--- acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/acceptance/AcceptanceTest.java
+++ acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/acceptance/AcceptanceTest.java
@@ -1,24 +1,19 @@
 package uk.gov.service.bluebadge.test.acceptance;
 
-import com.cucumber.listener.ExtentProperties;
-import com.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import java.io.IOException;
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
-import java.io.File;
-import java.io.IOException;
-
-
 /** Entry point required by {@code cucumber-jvm} to discover and run tests. */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features",
-        plugin = {"com.cucumber.listener.ExtentCucumberFormatter:output/report.html"})
+@CucumberOptions(
+  features = "src/test/resources/features",
+  plugin = {"com.cucumber.listener.ExtentCucumberFormatter:output/report.html"}
+)
 public class AcceptanceTest {
   // no-op, config class only
   @AfterClass
-  public static void teardown() throws IOException {
-
-      }
+  public static void teardown() throws IOException {}
 }
