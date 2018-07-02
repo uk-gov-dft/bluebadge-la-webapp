c5339aae52c46354802110baad23228c2ffb9ffd
Stephen Elliott <stephen@bealine.co.uk>
2018-07-02 08:37:30 +0100

    BBB-405 Tidy up

--- src/test/java/uk/gov/dft/bluebadge/webapp/la/integration/SecuredPageTests.java
+++ src/test/java/uk/gov/dft/bluebadge/webapp/la/integration/SecuredPageTests.java
@@ -1,19 +1,10 @@
 package uk.gov.dft.bluebadge.webapp.la.integration;
 
 import static com.jayway.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.startsWith;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static uk.gov.dft.bluebadge.webapp.la.controller.MockMVCWithSecurityTests.VALID_USERNAME_1;
 
 import org.apache.http.HttpHeaders;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
