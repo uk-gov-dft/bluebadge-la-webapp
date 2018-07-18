658919f66ea2893b908f321e03e779a86c6f0ede
Ali Ashik <ali.ashik@valtech.co.uk>
2018-07-18 16:22:31 +0100

    BBB-566 relpaced strings with enums to recognise reference data domains

--- src/main/java/uk/gov/dft/bluebadge/webapp/la/client/referencedataservice/ReferenceDataApiClient.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/client/referencedataservice/ReferenceDataApiClient.java
@@ -7,12 +7,12 @@ import org.springframework.stereotype.Service;
 import uk.gov.dft.bluebadge.webapp.la.client.RestTemplateFactory;
 import uk.gov.dft.bluebadge.webapp.la.client.referencedataservice.model.ReferenceData;
 import uk.gov.dft.bluebadge.webapp.la.client.referencedataservice.model.ReferenceDataResponse;
+import uk.gov.dft.bluebadge.webapp.la.service.referencedata.RefDataDomainEnum;
 
 @Slf4j
 @Service
 public class ReferenceDataApiClient {
 
-  public static final String REFERENCE_DATA_DOMAIN_BADGE = "BADGE";
   private final ReferenceDataServiceConfiguration messageServiceConfiguration;
   private final RestTemplateFactory restTemplateFactory;
 
@@ -29,7 +29,7 @@ public class ReferenceDataApiClient {
    *
    * @return List of reference data items.
    */
-  public List<ReferenceData> retrieveReferenceData() {
+  public List<ReferenceData> retrieveReferenceData(RefDataDomainEnum referenceDataDomain) {
     log.debug("Loading reference data.");
 
     ReferenceDataResponse response =
@@ -37,7 +37,7 @@ public class ReferenceDataApiClient {
             .getInstance()
             .getForEntity(
                 messageServiceConfiguration
-                    .getUriComponentsBuilder("reference-data", REFERENCE_DATA_DOMAIN_BADGE)
+                    .getUriComponentsBuilder("reference-data", referenceDataDomain.getDomain())
                     .toUriString(),
                 ReferenceDataResponse.class)
             .getBody();
