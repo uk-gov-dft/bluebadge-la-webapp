46f44be380174f04d8387d0917287fc117285d79
Miguel Gil <mgilgar@gmail.com>
2018-07-19 15:17:07 +0100

    [BBB-566-use-reference-data-for-add-a-badge] Sonarqube issues addressed

--- src/main/java/uk/gov/dft/bluebadge/webapp/la/client/badgemanagement/model/BadgeNumbersResponse.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/client/badgemanagement/model/BadgeNumbersResponse.java
@@ -71,14 +71,4 @@ public class BadgeNumbersResponse extends CommonResponse {
     sb.append("}");
     return sb.toString();
   }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces (except the first line).
-   */
-  private String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
 }
--- src/main/java/uk/gov/dft/bluebadge/webapp/la/client/badgemanagement/model/BadgeResponse.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/client/badgemanagement/model/BadgeResponse.java
@@ -59,14 +59,4 @@ public class BadgeResponse extends CommonResponse {
     sb.append("}");
     return sb.toString();
   }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces (except the first line).
-   */
-  private String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
 }
