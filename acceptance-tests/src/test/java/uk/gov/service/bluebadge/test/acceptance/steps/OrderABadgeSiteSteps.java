1ed6cff3d9261fe7ac30bdb3d13086d7329773a7
sampathmahavithana <sampath.mahavithana@valtech.co.uk>
2018-07-19 15:48:20 +0100

    Added new scenarios to acceptance tests

--- acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/acceptance/steps/OrderABadgeSiteSteps.java
+++ acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/acceptance/steps/OrderABadgeSiteSteps.java
@@ -22,12 +22,14 @@ public class OrderABadgeSiteSteps {
   protected LocalDateGenerator ldg = new LocalDateGenerator();
   protected PostCodeGenerator pcg = new PostCodeGenerator();
 
+
+
   @Autowired protected SitePage sitePage;
 
   @Autowired protected ScenarioContext scenarioContext;
 
   @When("^I enter all the mandatory valid personal details to order a badge$")
-  public void iEnterAllTheValidPersonalDetailsToOrderABadge() throws Throwable {
+  public void iEnterAllMandatoryValidPersonalDetailsToOrderABadge() throws Throwable {
     String name = ng.get_full_name();
     String contactName = ng.get_full_name();
     LocalDate date = ldg.get_local_date();
@@ -51,13 +53,60 @@ public class OrderABadgeSiteSteps {
     select.selectByVisibleText("PIP");
   }
 
+  @When("^I enter all valid personal details to order a badge$")
+  public void iEnterAllValidPersonalDetailsToOrderABadge() throws Throwable {
+    String name = ng.get_full_name();
+    String contactName = "Contact "+ name;
+    LocalDate date = ldg.get_local_date();
+    String email = ng.get_email(name);
+
+    String dobDay = String.valueOf(date.getDayOfMonth());
+    String dobMonth = String.valueOf(date.getMonth().getValue());
+    String dobYear = String.valueOf(date.getYear());
+    String postcode = pcg.get_postcode();
+
+    sitePage.findPageElementById("name").sendKeys(name);
+    sitePage.findPageElementById("male").click();
+    sitePage.findPageElementById("dobDay").sendKeys(dobDay);
+    sitePage.findPageElementById("dobMonth").sendKeys(dobMonth);
+    sitePage.findPageElementById("dobYear").sendKeys(dobYear);
+    sitePage.findElementWithUiPath("nino.field").sendKeys("SN234234C");
+    sitePage.findElementWithUiPath("buildingAndStreet.field").sendKeys("building and street");
+    sitePage.findElementWithUiPath("optionalAddressField.field").sendKeys("second line of address");
+    sitePage.findElementWithUiPath("townOrCity.field").sendKeys("Town or city");
+    sitePage.findElementWithUiPath("postcode.field").sendKeys(postcode);
+    sitePage.findElementWithUiPath("contactDetailsName.field").sendKeys(contactName);
+    sitePage.findElementWithUiPath("contactDetailsContactNumber.field").sendKeys("020 7014 0800");
+    sitePage.findElementWithUiPath("contactDetailsEmailAddress.field").sendKeys(email);
+
+    Select select = new Select(sitePage.findPageElementById("eligibility"));
+    select.selectByVisibleText("PIP");
+  }
+
   @When("^I enter all the mandatory valid processing details to order a badge$")
-  public void iEnterAllTheValidProcessingDetailsToOrderABadge() throws Throwable {
+  public void iEnterAllMandatoryValidProcessingDetailsToOrderABadge() throws Throwable {
     sitePage.findElementWithUiPath("applicationDate.day.field").sendKeys("1");
     sitePage.findElementWithUiPath("applicationDate.month.field").sendKeys("6");
     sitePage.findElementWithUiPath("applicationDate.year.field").sendKeys("2018");
     sitePage.findPageElementById("online").click();
     sitePage.findElementWithUiPath("badgeStartDate.day.field").sendKeys("1");
+    sitePage.findElementWithUiPath("badgeStartDate.month.field").sendKeys("5");
+    sitePage.findElementWithUiPath("badgeStartDate.year.field").sendKeys("2025");
+    sitePage.findElementWithUiPath("badgeExpiryDateValid.day.field").sendKeys("1");
+    sitePage.findElementWithUiPath("badgeExpiryDateValid.month.field").sendKeys("5");
+    sitePage.findElementWithUiPath("badgeExpiryDateValid.year.field").sendKeys("2028");
+    sitePage.findPageElementById("badgeHolder").click();
+    sitePage.findPageElementById("standard").click();
+  }
+
+  @When("^I enter all valid processing details to order a badge$")
+  public void iEnterAllValidProcessingDetailsToOrderABadge() throws Throwable {
+    sitePage.findElementWithUiPath("applicationDate.day.field").sendKeys("1");
+    sitePage.findElementWithUiPath("applicationDate.month.field").sendKeys("6");
+    sitePage.findElementWithUiPath("applicationDate.year.field").sendKeys("2018");
+    sitePage.findPageElementById("online").click();
+    sitePage.findElementWithUiPath("badgeStartDate.day.field").sendKeys("1");
+    sitePage.findElementWithUiPath("localAuthorityReferenceNumber.field").sendKeys("Manchester City Council");
     sitePage.findElementWithUiPath("badgeStartDate.month.field").sendKeys("5");
     sitePage.findElementWithUiPath("badgeStartDate.year.field").sendKeys("2025");
     sitePage.findElementWithUiPath("badgeExpiryDateValid.day.field").sendKeys("1");
