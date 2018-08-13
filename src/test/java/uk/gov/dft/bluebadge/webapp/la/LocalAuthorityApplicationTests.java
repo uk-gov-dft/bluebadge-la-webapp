aaf3fc93d56554d5cac65497977517117009722e
Paul Roberts <paul.roberts@valtech.co.uk>
2018-08-13 15:03:54 +0100

    Add port to context in unit teests

--- src/test/java/uk/gov/dft/bluebadge/webapp/la/LocalAuthorityApplicationTests.java
+++ src/test/java/uk/gov/dft/bluebadge/webapp/la/LocalAuthorityApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(properties = {"management.server.port=19990"})
 public class LocalAuthorityApplicationTests {
 
   @Test
