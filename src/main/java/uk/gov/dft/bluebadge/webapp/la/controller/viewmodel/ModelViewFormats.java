70ed5ff5437f012feafd609ea8e93747bf5cdcb0
Miguel Gil <mgilgar@gmail.com>
2018-08-23 11:46:22 +0100

    [BBB-254-view-applications] sonar qube.

--- src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/viewmodel/ModelViewFormats.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/viewmodel/ModelViewFormats.java
@@ -3,6 +3,9 @@ package uk.gov.dft.bluebadge.webapp.la.controller.viewmodel;
 import java.time.format.DateTimeFormatter;
 
 public class ModelViewFormats {
+
+  private ModelViewFormats() {};
+
   private static final String VIEW_MODEL_DATE_FORMAT = "dd/MM/yy";
 
   private static final String VIEW_MODEL_DATE_TIME_FORMAT = "dd/MM/yy hh:mm";
