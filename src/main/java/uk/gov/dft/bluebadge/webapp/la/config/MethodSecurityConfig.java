a4aa928af38511ff00074a063a42c04a01645e4b
Miguel Gil <mgilgar@gmail.com>
2018-09-18 18:27:43 +0100

    [BBB-122-implement-user-roles-in-front-end-left-hand-menu] Fix Jenkinsfile

--- src/main/java/uk/gov/dft/bluebadge/webapp/la/config/MethodSecurityConfig.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/config/MethodSecurityConfig.java
@@ -6,6 +6,4 @@ import org.springframework.security.config.annotation.method.configuration.Globa
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
-
-}
+public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {}
