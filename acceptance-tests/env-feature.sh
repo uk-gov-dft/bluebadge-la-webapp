3e8d5abcf2261227534ff4ef921a740fbff0c3f6
Miguel Gil <mgilgar@gmail.com>
2018-09-18 19:10:56 +0100

    [BBB-122-implement-user-roles-in-front-end-left-hand-menu] Fix env-feature.sh

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -2,5 +2,4 @@
 
 # Override the version with the branch name
 export LA_VERSION=$(cat VERSION-computed)
-export UM_VERSION=0.7.0-feature_BBB-631-role-permissions
-export AZ_VERSION=0.7.0-feature_BBB-631-role-permissions
+
