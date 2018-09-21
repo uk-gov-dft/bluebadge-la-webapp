3e0e04180ab0b05bdc6d1c5a2afd55fc2ced5837
xytrams <yuliya.polyeno@gmail.com>
2018-09-21 08:43:59 +0100

    BBB-833 wip

--- src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/request/UserFormRequest.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/request/UserFormRequest.java
@@ -1,12 +1,16 @@
 package uk.gov.dft.bluebadge.webapp.la.controller.request;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class UserFormRequest {
   private String localAuthorityShortCode;
+  @NotBlank(message = "{NotNull.user.emailAddress}")
   private String emailAddress;
+  @NotBlank(message = "{NotNull.user.name}")
   private String name;
-  // @NotBlank(message = "{NotNull.badge.applicantType}")
+  @NotBlank(message = "{NotNull.user.roleName}")
   private String roleName;
 }
