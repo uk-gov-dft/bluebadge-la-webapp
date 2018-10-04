6150b944f24ccd739515ab3553d86694a8beb8bb
xytrams <yuliya.polyeno@gmail.com>
2018-10-04 14:28:37 +0100

    BBB-909 remove broken character

--- acceptance-tests/run_local.sh
+++ acceptance-tests/run_local.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gradle acceptanceTests -Dheadless=false -DbaseUrl=http://localhost:8080
+gradle acceptanceTests -Dheadless=false -DbaseUrl=http://localhost:8080 -Dcucumber.options="--tags @SignOut"
