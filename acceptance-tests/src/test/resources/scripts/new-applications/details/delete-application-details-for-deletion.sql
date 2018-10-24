1058c9a17bec63a24d8017089fa445921c396388
Miguel Gil <mgilgar@gmail.com>
2018-10-24 10:52:53 +0100

    [BBB-348-delete-applications] Review comments addressed

--- acceptance-tests/src/test/resources/scripts/new-applications/details/delete-application-details-for-deletion.sql
+++ acceptance-tests/src/test/resources/scripts/new-applications/details/delete-application-details-for-deletion.sql
@@ -1,4 +1,3 @@
 SET search_path TO applicationmanagement;
 
--- Use an invalid postcode and name to delete test data
 DELETE FROM applicationmanagement.application WHERE id = '1087ac26-491a-46f0-9006-36187dc40771'::uuid;
