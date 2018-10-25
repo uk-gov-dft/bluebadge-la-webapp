9bf8b80f92f5abe8562cda00b3e8658b0a27384e
Ali Ashik <ali.ashik@valtech.co.uk>
2018-10-25 11:17:39 +0100

    BBB-940 created customAccessDenied Handler to handle 403 errors

---
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,32 @@
+package uk.gov.dft.bluebadge.webapp.la.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private static Logger logger = LoggerFactory.getLogger(CustomAccessDeniedHandler.class);
+
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException exc) throws IOException {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null) {
+            logger.info("User '{}' attempted to access the protected URL: {}", auth.getName(), request.getRequestURI());
+        }
+
+        response.sendRedirect(request.getContextPath() + "/something-went-wrong");
+    }
+}
