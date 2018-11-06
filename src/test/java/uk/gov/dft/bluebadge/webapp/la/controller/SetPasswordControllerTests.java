9d058a766215da889c92b0f7970ef8a34b9ec9a0
Paul Roberts <paul.roberts@valtech.co.uk>
2018-11-06 12:57:23 +0000

    BBB-959 fix unit tests

--- src/test/java/uk/gov/dft/bluebadge/webapp/la/controller/SetPasswordControllerTests.java
+++ src/test/java/uk/gov/dft/bluebadge/webapp/la/controller/SetPasswordControllerTests.java
@@ -1,16 +1,35 @@
 package uk.gov.dft.bluebadge.webapp.la.controller;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.UUID;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import uk.gov.dft.bluebadge.webapp.la.BaseSpringBootTest;
+import uk.gov.dft.bluebadge.webapp.la.client.RestTemplateFactory;
 
 @RunWith(SpringRunner.class)
-public class SetPasswordControllerTests extends MockMVCWithSecurityTests {
+public class SetPasswordControllerTests extends BaseSpringBootTest {
+
+  @Autowired
+  private WebApplicationContext wac;
+  private MockMvc mockMvc;
+
+  @Before
+  public void setupMockMVC() {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).apply(springSecurity()).build();
+  }
 
   @Test
   public void givenNoAuth_whenGetSetPasswordRequest_thenSuccess() throws Exception {
