e39964cb8e5200b0a717b062e1aa7bfdc0cf4114
Robert Smallwood <38761367+robsvaltech@users.noreply.github.com>
2018-11-28 15:57:40 +0000

    BBB-1039 - Changed view of health conditions (#152)
    * BBB-1039 - Changed view of health conditions

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -3,12 +3,5 @@
 # Override the version with the branch name
 export LA_VERSION=$(cat VERSION-computed)
 
-export AP_VERSION=v0.10.0-rc1
-export AZ_VERSION=v0.12.0-rc1
-
-export UM_VERSION=v0.14.0-rc1
-export MG_VERSION=v0.9.0-rc1
-export RD_VERSION=v0.12.0-rc1
-export BB_VERSION=v0.11.0-rc1
 
 
