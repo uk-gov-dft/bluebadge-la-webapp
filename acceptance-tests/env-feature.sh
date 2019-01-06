a3376a3bdc8ebddc50a6bc7d625786388fbb2807
Miguel Gil <mgilgar@gmail.com>
2019-01-06 14:47:14 +0000

    [BBB-1097-view-local-authorities] change env-feature.sh

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -3,4 +3,4 @@
 # Override the version with the branch name
 export LA_VERSION=$(cat VERSION-computed)
 
-export AP_VERSION=v0.11.0-rc1-feature_BBB-1069-pagination
+export export AZ_VERSION=0.6.10-feature_BBB-1097-view-local-authorities-SNAPSHOT
