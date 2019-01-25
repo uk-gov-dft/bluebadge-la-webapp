20b9c1fffbfc40b8a94fc92c9555c3088326884d
Rob Pataki <robert.pataki@valtech.com>
2019-01-25 14:06:05 +0000

    BBB-1081 Set default application channel to ONLINE

--- src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/converter/ApplicationToOrderBadgeProcessingFormRequest.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/converter/ApplicationToOrderBadgeProcessingFormRequest.java
@@ -4,8 +4,6 @@ import java.time.OffsetDateTime;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
-import uk.gov.dft.bluebadge.webapp.la.client.applications.model.AppContact;
-import uk.gov.dft.bluebadge.webapp.la.client.applications.model.AppPerson;
 import uk.gov.dft.bluebadge.webapp.la.client.applications.model.Application;
 import uk.gov.dft.bluebadge.webapp.la.controller.request.orderbadge.OrderBadgeProcessingFormRequest;
 
@@ -13,18 +11,19 @@ import uk.gov.dft.bluebadge.webapp.la.controller.request.orderbadge.OrderBadgePr
 public class ApplicationToOrderBadgeProcessingFormRequest
     implements Converter<Application, OrderBadgeProcessingFormRequest> {
 
+  private static final String DEFAULT_APPLICATION_CHANNEL = "ONLINE";
+
   @Override
   public OrderBadgeProcessingFormRequest convert(Application source) {
     Assert.notNull(source, "Source cannot be null");
 
-    AppPerson appPerson = source.getParty().getPerson();
-    AppContact appContact = source.getParty().getContact();
     OffsetDateTime submissionDate = source.getSubmissionDate();
 
     return OrderBadgeProcessingFormRequest.builder()
         .applicationDateDay(submissionDate.getDayOfMonth())
         .applicationDateMonth(submissionDate.getMonthValue())
         .applicationDateYear(submissionDate.getYear())
+        .applicationChannel(DEFAULT_APPLICATION_CHANNEL)
         .build();
   }
 }
