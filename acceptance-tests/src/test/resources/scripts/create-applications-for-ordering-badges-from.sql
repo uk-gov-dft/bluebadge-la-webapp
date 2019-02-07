c395a618868d959449f9ec2c8e47a24f1c0242d2
Paul Roberts <paul.roberts@valtech.com>
2019-02-07 10:33:09 +0000

    BBB-1163 fix cleanup test data

--- acceptance-tests/src/test/resources/scripts/create-applications-for-ordering-badges-from.sql
+++ acceptance-tests/src/test/resources/scripts/create-applications-for-ordering-badges-from.sql
@@ -1,6 +1,10 @@
 SET search_path TO applicationmanagement;
 
-DELETE FROM applicationmanagement.application WHERE id IN ('1087ac26-491a-46f0-9006-36187dc40764'::uuid, '1087ac26-491a-46f0-9006-36187dc40767'::uuid);
+DELETE FROM applicationmanagement.app_artifact
+WHERE application_id IN ('1087ac26-491a-46f0-9006-36187dc40764'::uuid, '1087ac26-491a-46f0-9006-36187dc40767'::uuid);
+DELETE FROM applicationmanagement.application
+WHERE id IN ('1087ac26-491a-46f0-9006-36187dc40764'::uuid, '1087ac26-491a-46f0-9006-36187dc40767'::uuid);
+
 INSERT INTO applicationmanagement.application(
    id, local_authority_code, app_type_code,
    is_payment_taken, submission_datetime, party_code,
