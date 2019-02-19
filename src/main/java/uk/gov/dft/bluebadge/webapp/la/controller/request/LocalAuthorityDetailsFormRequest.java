3f6b35ebf72c9bbc1df2eadab44aa3d9775f7610
Rob Pataki <robert.pataki@valtech.com>
2019-02-19 11:53:02 +0000

    BBB-1213 Mark optional fields in the UI + add validation

--- src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/request/LocalAuthorityDetailsFormRequest.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/request/LocalAuthorityDetailsFormRequest.java
@@ -6,9 +6,12 @@ import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.Builder;
 import lombok.Data;
 import org.hibernate.validator.constraints.URL;
+import uk.gov.dft.bluebadge.common.util.ValidationPattern;
 
 @Data
 @Builder
@@ -18,24 +21,36 @@ public class LocalAuthorityDetailsFormRequest implements Serializable {
 
   private String welshDescription;
 
+  @Size(min = 1, max = 40, message = "{Size.localAuthorityDetailPage.nameLine2}")
   private String nameLine2;
 
+  @Size(min = 1, max = 40, message = "{Size.localAuthorityDetailPage.addressLine1}")
   private String addressLine1;
 
+  @Size(min = 1, max = 40, message = "{Size.localAuthorityDetailPage.addressLine2}")
   private String addressLine2;
 
+  @Size(min = 1, max = 40, message = "{Size.localAuthorityDetailPage.addressLine3}")
   private String addressLine3;
 
+  @Size(min = 1, max = 40, message = "{Size.localAuthorityDetailPage.addressLine4}")
   private String addressLine4;
 
+  @Size(min = 1, max = 40, message = "{Size.localAuthorityDetailPage.town}")
   private String town;
 
+  @Size(min = 1, max = 40, message = "{Size.localAuthorityDetailPage.county}")
   private String county;
 
   @NotBlank(message = "{NotBlank.localAuthorityDetailPage.postcode}")
+  @Pattern(
+          regexp = ValidationPattern.POSTCODE_CASE_INSENSITIVE,
+          message = "{Pattern.localAuthorityDetailPage.postcode}"
+  )
   private String postcode;
-
+  
   @NotBlank(message = "{NotBlank.localAuthorityDetailPage.country}")
+  @Size(min = 1, max = 40, message = "{Size.localAuthorityDetailPage.country}")
   private String country;
 
   @NotBlank(message = "{NotBlank.localAuthorityDetailPage.nation}")
@@ -45,12 +60,20 @@ public class LocalAuthorityDetailsFormRequest implements Serializable {
   )
   private String nation;
 
+  @Pattern(
+          regexp = ValidationPattern.PHONE_NUMBER,
+          message = "{Pattern.localAuthorityDetailPage.contactNumber}"
+  )
   private String contactNumber;
 
   @NotBlank(message = "{NotBlank.localAuthorityDetailPage.websiteUrl}")
   @URL(message = "{URL.localAuthorityDetailPage.websiteUrl}")
   private String websiteUrl;
 
+  @Pattern(
+          regexp = ValidationPattern.EMAIL,
+          message = "{Pattern.localAuthorityDetailPage.emailAddress}"
+  )
   private String emailAddress;
 
   private String badgePackType;
