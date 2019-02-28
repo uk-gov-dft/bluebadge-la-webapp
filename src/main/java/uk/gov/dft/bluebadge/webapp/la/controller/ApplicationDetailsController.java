39dd50c28b27c6f7f7f66986ee1f65d4fc8665ce
Rob Pataki <robert.pataki@valtech.com>
2019-02-28 16:53:13 +0000

    BBB-1146 Implement eligibility type condiotionals

--- src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/ApplicationDetailsController.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/ApplicationDetailsController.java
@@ -14,9 +14,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import uk.gov.dft.bluebadge.webapp.la.client.applications.model.Application;
+import uk.gov.dft.bluebadge.webapp.la.client.applications.model.Eligibility;
 import uk.gov.dft.bluebadge.webapp.la.client.applications.model.EligibilityCodeField;
 import uk.gov.dft.bluebadge.webapp.la.client.applications.model.PartyTypeCodeField;
 import uk.gov.dft.bluebadge.webapp.la.service.ApplicationService;
+import uk.gov.dft.bluebadge.webapp.la.service.referencedata.ReferenceDataService;
 
 @Controller
 @RequestMapping(path = "/new-applications/{uuid}")
@@ -28,21 +30,36 @@ public class ApplicationDetailsController {
       "redirect:" + NewApplicationsController.URL;
 
   private ApplicationService applicationService;
+  private ReferenceDataService referenceDataService;
 
   @Autowired
-  public ApplicationDetailsController(ApplicationService applicationService) {
+  public ApplicationDetailsController(ApplicationService applicationService, ReferenceDataService referenceDataService) {
     this.applicationService = applicationService;
+    this.referenceDataService = referenceDataService;
   }
 
   @GetMapping()
   public String show(@PathVariable(PARAM_UUID) UUID uuid, Model model) {
     Application application = applicationService.retrieve(uuid.toString());
-
-    model.addAttribute("altHealthConditionLabel", useAlternativeConditionLabel(application));
-    model.addAttribute("app", application);
-    model.addAttribute("uuid", uuid);
-    model.addAttribute(
+      model.addAttribute("altHealthConditionLabel", useAlternativeConditionLabel(application));
+      model.addAttribute("app", application);
+      model.addAttribute("uuid", uuid);
+      model.addAttribute(
         "renderOrderBadgeButton", application.getParty().getTypeCode() != PartyTypeCodeField.ORG);
+
+    if (application.getEligibility() != null) {
+      String eligibilityTypeCode = application.getEligibility().getTypeCode().toString();
+      String supportingDocsEligibilityTypesPattern = "^(ARMS|WALKD|CHILDVEHIC|CHILDBULK)$";
+      String hcpEligibilityTypesPattern = "^(WALKD|CHILDVEHIC|CHILDBULK)$";
+      String proofOfEligibilityEligibilityTypesPattern = "^(PIP|DLA)$";
+
+      model.addAttribute("renderSupportingDocs", eligibilityTypeCode
+              .matches(supportingDocsEligibilityTypesPattern) ? true : false);
+      model.addAttribute("renderHCP", eligibilityTypeCode
+              .matches(hcpEligibilityTypesPattern) ? true : false);
+      model.addAttribute("renderProofOfEligibility", eligibilityTypeCode
+              .matches(proofOfEligibilityEligibilityTypesPattern) ? true : false);
+    }
 
     return TEMPLATE;
   }
