d09e1bcf2bfb34513b0171b7d73bc8f4bb46d6ff
Miguel Gil <mgilgar@gmail.com>
2019-02-28 16:06:14 +0000

    [BBB-1235-display-and-update-application-status] refactor govuk-radio-button

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -2,4 +2,5 @@
 
 # Override the version with the branch name
 export LA_VERSION=$(cat VERSION-computed)
+export AP_VERSION=v0.15.1-feature_BBB-1234-application-status
 export RD_VERSION=v0.17.0-feature_BBB-1235-display-and-update-application-status
