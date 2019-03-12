e20b15b9a795dda15ed2ab06e8ef39e4c7b7ca9e
Miguel Gil <mgilgar@gmail.com>
2019-03-12 14:01:00 +0000

    [BBB-1289-proof-of-benefit-and-supporting-documents-not-displayed-in-LA-web-app] Update runtime dependency once the dependency is in develop

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -2,4 +2,3 @@
 
 # Override the version with the branch name
 export LA_VERSION=$(cat VERSION-computed)
-export BB_VERSION=v0.18.0-feature_BBB-1273-add-issued-date-and-rejected-reason-to-GET-badges
