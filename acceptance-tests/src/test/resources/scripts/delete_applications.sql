72a2cb1a208e8dfa7320a097f0192269c11b777e
Gerald Anene <Gerald.Anene@valtech.com>
2019-03-21 09:26:58 +0000

    Removed delete script from applications test

--- acceptance-tests/src/test/resources/scripts/delete_applications.sql
+++ acceptance-tests/src/test/resources/scripts/delete_applications.sql
@@ -5,8 +5,3 @@ DELETE FROM applicationmanagement.application WHERE id IN ('1087ac26-491a-46f0-9
 														 '1087ac26-491a-46f0-9006-36187dc40765'::uuid,
 														 '1087ac26-491a-46f0-9006-36187dc40766'::uuid,
 														 '1087ac26-491a-46f0-9006-36187dc40767'::uuid);
-
-DELETE FROM applicationmanagement.app_breathlessness_type WHERE application_id IN ('1087ac26-491a-46f0-9006-36187dc40764'::uuid,
-                                                         														 '1087ac26-491a-46f0-9006-36187dc40765'::uuid,
-                                                         														 '1087ac26-491a-46f0-9006-36187dc40766'::uuid,
-                                                         														 '1087ac26-491a-46f0-9006-36187dc40767'::uuid);
