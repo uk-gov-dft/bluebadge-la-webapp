ddbe77af50ded5e237e345a99f828926c92990e4
Rob Pataki <robert.pataki@valtech.com>
2019-03-27 09:55:43 +0000

    BBB-1152 Add translation for error message

--- src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/request/TransferApplicationFormRequest.java
+++ src/main/java/uk/gov/dft/bluebadge/webapp/la/controller/request/TransferApplicationFormRequest.java
@@ -4,8 +4,11 @@ import java.io.Serializable;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 public class TransferApplicationFormRequest implements Serializable {
+  @NotBlank(message = "{NotNull.application.details.transfer.la}")
   private String transferToLaShortCode;
 }
