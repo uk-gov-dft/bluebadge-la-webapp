862929d159650601f031e8fadba4c39a4c7d25d0
Miguel Gil <mgilgar@gmail.com>
2019-04-09 17:01:52 +0100

    [BBB-1348-accept-10-character-phone-numbers-for-applications-and-badges] update env-feature

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -2,3 +2,5 @@
 
 # Override the version with the branch name
 export LA_VERSION=$(cat VERSION-computed)
+export BB_VERSION=v0.21.0-feature_BBB-1348-accept-10-character-phone-numbers-for-applications-and-badges
+export RD_VERSION=v0.21.0-feature_BBB-1348-accept-10-character-phone-numbers-for-applications-and-badges
