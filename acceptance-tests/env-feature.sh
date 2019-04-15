ba708847bf7449b603b863ee3b40978efb889ac6
Rob Pataki <robert.pataki@valtech.com>
2019-04-15 12:20:49 +0100

    Remove API dependencies

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -2,5 +2,3 @@
 
 # Override the version with the branch name
 export LA_VERSION=$(cat VERSION-computed)
-export AP_VERSION=v0.20.0-feature_BBB-1339-add-walking-times
-export RD_VERSION=v0.21.0-feature_BBB-1339-add-walking-times
