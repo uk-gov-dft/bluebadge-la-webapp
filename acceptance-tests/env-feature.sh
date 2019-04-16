e2ea1f0a2142861909b1b7ba4f55761f534bfd06
Paul Roberts <paul.roberts@valtech.com>
2019-04-16 16:12:55 +0100

    BBB-1321 env feature updates

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -2,3 +2,10 @@
 
 # Override the version with the branch name
 export LA_VERSION=$(cat VERSION-computed)
+export RD_VERSION=v0.22.0-feature_BBB-1321-api-version-accept-header
+export MG_VERSION=v0.14.0-feature_BBB-1321-api-version-accept-header
+export PR_VERSION=v0.8.0-feature_BBB-1321-api-version-accept-header
+export UM_VERSION=v0.20.0-feature_BBB-1321-api-version-accept-header
+export PY_VERSION=v0.7.0-feature_BBB-1321-api-version-accept-header
+export BB_VERSION=v0.22.0-feature_BBB-1321-api-version-accept-header
+export AP_VERSION=v0.21.0-feature_BBB-1321-api-version-accept-header
