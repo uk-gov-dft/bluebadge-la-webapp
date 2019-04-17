8b6b3f0842ae7fc0a4590f0504e079fce4054f05
Miguel Gil <mgilgar@gmail.com>
2019-04-17 09:46:51 +0100

    [BBB-1355-new-applications-page-improvements] env-feature.sh update

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -2,3 +2,5 @@
 
 # Override the version with the branch name
 export LA_VERSION=$(cat VERSION-computed)
+export AP_VERSION=v0.21.0-feature_BBB-1355-new-applications-page-improvements
+export RD_VERSION=v0.22.0-feature_BBB-1355-new-applications-page-improvements
