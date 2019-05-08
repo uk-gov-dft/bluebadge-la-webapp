810260b935cd5ded4311f8d4e91972df9b1d416b
Rob Pataki <robert.pataki@valtech.com>
2019-05-08 14:56:47 +0100

    BBB-1365 Format code

--- src/test/java/uk/gov/dft/bluebadge/webapp/la/controller/ApplicationsControllerTest.java
+++ src/test/java/uk/gov/dft/bluebadge/webapp/la/controller/ApplicationsControllerTest.java
@@ -15,7 +15,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.Collections;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
