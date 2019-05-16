ad337d57bc4f0487adfe36401185a73df1778623
Miguel Gil <mgilgar@gmail.com>
2019-05-16 14:50:24 +0100

    [BBB-1364-mobile-view-improvements] update runtime dependency

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -2,3 +2,4 @@
 
 # Override the version with the branch name
 export LA_VERSION=$(cat VERSION-computed)
+export RD_VERSION=v0.25.0-feature_BBB-1373-add-streamlined-reassessment-disabled-flag-to-manage-local-authorities
