7403d085deb21c39e109fe8a1238e2b790299aa1
Miguel Gil <mgilgar@gmail.com>
2019-05-20 15:11:51 +0100

    [BBB-1364-mobile-view-improvements] bug fixes.

--- src/test/java/uk/gov/dft/bluebadge/webapp/la/controller/converter/servicetoviewmodel/BadgeToBadgeDetailsViewModelTest.java
+++ src/test/java/uk/gov/dft/bluebadge/webapp/la/controller/converter/servicetoviewmodel/BadgeToBadgeDetailsViewModelTest.java
@@ -19,6 +19,7 @@ import uk.gov.dft.bluebadge.webapp.la.service.referencedata.ReferenceDataService
 
 public class BadgeToBadgeDetailsViewModelTest {
 
+  private static final String PARTY_TYPE_CODE_PERSON = "PERSON";
   private static final String APPLICATION_CHANNEL = "CHANNEL";
   private static final String APPLICANT_AGE = "89";
   private static final LocalDate APPLICATION_DATE = LocalDate.of(2017, 1, 1);
@@ -97,6 +98,7 @@ public class BadgeToBadgeDetailsViewModelTest {
 
   private static final BadgeDetailsViewModel BADGE_DETAILS_VIEW_MODEL =
       BadgeDetailsViewModel.builder()
+          .partyTypeCode(PARTY_TYPE_CODE_PERSON)
           .applicationChannel(APPLICATION_CHANNEL_VIEW_MODEL)
           .applicationDate(APPLICATION_DATE_VIEW_MODEL)
           .age(APPLICANT_AGE)
